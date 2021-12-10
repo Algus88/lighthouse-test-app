25da9a0edaf7cf51f76fb53ba867d9b1b9ed4426
Oleksii <41671732+Algus88@users.noreply.github.com>
2021-12-10 12:41:44 +0200

    Update lighthouserc.js

--- lighthouserc.js
+++ lighthouserc.js
@@ -1,10 +1,29 @@
 module.exports = {
   ci: {
-    collect: {
+       collect: {
       url: ['https://gamesglobal.com/'],
+      numberOfRuns: 3,
+	    settings: {
+        configPath: "./desktop_config.js"
+      }
+    },
+    assert: {
+      assertions: {
+		      'categories.performance' : ["warn", {"minScore": 0.8, "aggregationMethod": "optimistic"}],
+        	'categories.accessibility' : ["warn", {"minScore": 0.8}],
+		      'categories.seo' : ["warn", {"minScore":  0.8}],
+        	'categories.best-practices' : ["warn", {"minScore":  0.8}],
+        	'categories.pwa' : "off"
+      }
     },
     upload: {
       target: 'temporary-public-storage',
+    },
+    server: {
+      // server options here
+    },
+    wizard: {
+      // wizard options here
     },
   },
 };
