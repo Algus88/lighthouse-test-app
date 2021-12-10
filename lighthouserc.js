b1268fec4b10a0e13ba9284320b40911b5856a5e
Oleksii <41671732+Algus88@users.noreply.github.com>
2021-12-10 14:58:15 +0200

    Update lighthouserc.js

--- lighthouserc.js
+++ lighthouserc.js
@@ -19,7 +19,7 @@ module.exports = {
     upload: {
       //target: 'temporary-public-storage'
 	target: 'filesystem',
-	outputDir:'.'
+	outputDir:'./lhci'
     },
     server: {
       // server options here
