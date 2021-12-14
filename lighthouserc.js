44d14df7b9380b26cb012e10f6c0ca50d4e32a25
Oleksii <41671732+Algus88@users.noreply.github.com>
2021-12-14 10:34:56 +0200

    Update lighthouserc.js

--- lighthouserc.js
+++ lighthouserc.js
@@ -1,13 +1,16 @@
 module.exports = {
   ci: {
        collect: {
-      url: ['https://gamesglobal.com/'],
+      url: ['https://gamesglobal.com/',
+	    'https://gamesglobal.com/OurBusiness',
+	    'https://gamesglobal.com/Media',
+	    'https://gamesglobal.com/ContactUs'
+	   ],
       numberOfRuns: 3
     },
     assert: {
       assertions: {
-		'categories.performance' : ["error", {"minScore": 0.8, "aggregationMethod": "pessimistic"}],
-	        'categories.performance' : ["warn", {"minScore": 0.4, "aggregationMethod": "pessimistic"}],
+		'categories.performance' : ["error", {"minScore": 0.8, "aggregationMethod": "pessimistic"}],     
         	'categories.accessibility' : ["error", {"minScore": 0.7, "aggregationMethod": "optimistic"}],
 		'categories.seo' : ["error", {"minScore":  0.6, "aggregationMethod": "median-run"}],
         	'categories.best-practices' : ["error", {"minScore":  0.7, "aggregationMethod": "median"}],
